aaee4d07808a6473413cfa7d7e47a8b7283671db
cao.huchen <cao.huchen@hzleaper.com>
2025-05-26 13:57:13 +0800

    test

--- pythonTools/resizeImgs.py
+++ pythonTools/resizeImgs.py
@@ -9,7 +9,7 @@ def resize_images(source_dir, target_dir, output_size=(256,256)):
         for file in files:
             if file.lower().endswith(('.png', '.bmp', '.jpg', '.jpeg', '.gif')):
                 with Image.open(os.path.join(root, file)) as img:
-                    img = img.resize(output_size, Image.ANTIALIAS)
+                    img = img.resize(output_size, Image.Resampling.LANCZOS)
 
                     relative_path = os.path.relpath(root, source_dir)
                     target_subdir = os.path.join(target_dir, relative_path)
@@ -23,9 +23,9 @@ def resize_images(source_dir, target_dir, output_size=(256,256)):
 
 
 if __name__ == "__main__":
-    source_path = r"F:\dataSet\PE\bigError_BM_XS_BlackGround"
-    target_path = r"F:\dataSet\PE\bigError_BM_XS_BlackGround256"
-    output_size = (256, 256)
+    source_path = r"D:\code_study\my_tools\pythonTools\output\2025-05-23-17"
+    target_path = r"F:\dataSet\PE\YBDC\刻蚀印\matting\data512"
+    output_size = (512, 512)
     resize_images(source_path, target_path, output_size)
 
 
--- pythonTools/splitDataSet.py
+++ pythonTools/splitDataSet.py
@@ -11,11 +11,11 @@ import splitfolders
 import time
 import pathlib as path
 # 指定你的数据集所在的文件夹路径
-dataset_path = r'F:\dataSet\PE\YBDC\刻蚀印\matting'
+dataset_path = r'F:\dataSet\PE\桐城中环\matting'
 dataset_path = path.Path(dataset_path)
 # 目标文件夹
 dest_path = 'output'
 todayTime = time.strftime("%Y-%m-%d-%H", time.localtime())
 output = "./output/" + todayTime
 # 调用 split 方法进行分割
-splitfolders.ratio(dataset_path, output=output, ratio=(.8, .1, .1), group_prefix=None, move=False)
+splitfolders.ratio(dataset_path, output=output, ratio=(.8, .2), group_prefix=None, move=False)
